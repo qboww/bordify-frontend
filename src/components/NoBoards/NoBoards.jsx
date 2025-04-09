c14d25aac74515ae6c7c20a5e8297da8c020c5f9
Yevhenii Sarancha <yevhenii.sarancha@sigma.software>
2025-04-09 23:26:08 +0300

    minor changes

--- src/components/NoBoards/NoBoards.jsx
+++ src/components/NoBoards/NoBoards.jsx
@@ -16,11 +16,8 @@ export const NoBoards = () => {
   return (
     <div className={s.no_boards_wrap}>
       <p className={s.no_boards}>
-        Before starting your project, it is essential to
-        <a href="#!" onClick={openModal}> create a board </a>
-        to visualize and track all the necessary tasks and milestones. This
-        board serves as a powerful tool to organize the workflow and ensure
-        effective collaboration among team members.
+        To ensure your project runs smoothly, begin by
+        <a href="#!" onClick={openModal}> creating a board! </a>
       </p>
 
       {isOpen && (
