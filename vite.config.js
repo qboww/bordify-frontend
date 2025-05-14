c384b5a9c04563b8b5d893f0a92b5f9450ad4004
Yevhenii Sarancha <yevhenii.sarancha@sigma.software>
2025-05-14 18:45:29 +0300

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react({
-    reactRefresh: true, 
-  })],
+  plugins: [
+    react({ reactRefresh: true }),
+    visualizer({
+      open: true,
+      filename: "stats.html",
+      gzipSize: true,
+      brotliSize: true,
+      template: "treemap",
+      // Remove the `exclude` option for now
+    }),
+  ],
   build: {
     sourcemap: true,
+    chunkSizeWarningLimit: 1000,
   },
   base: "/bordify"
 });
